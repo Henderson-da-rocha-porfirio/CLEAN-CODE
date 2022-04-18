6bbd45a46798b0bfd153d84e878298a516a622be
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-04-18 12:02:28 -0300

    #COMMIT 6 - cleancode

--- src/main/java/name/intentionrevealingname/cleancode/NamesArrayList.java
+++ src/main/java/name/intentionrevealingname/cleancode/NamesArrayList.java
@@ -2,7 +2,14 @@ package src.main.java.name.intentionrevealingname.cleancode;
 
 // Notice that the simplicity of the code has not changed. It still has exactly the same number of operators and constants, with exactly the same number of nesting levels.
 
-/*public List< int[]>getFlaggedCells(){
+/*We can go further and write a simple class for cells instead of using an array of ints. It can include an intention-revealing function
+        (call it isFlagged) to hide the magic numbers. It results in a new version of the function:*/
+
+
+
+public class NamesArrayList {
+
+    /*public List< int[]>getFlaggedCells(){
     List< int[]>flaggedCells=new ArrayList< int[]>();
     for(int[]cell:gameBoard)
         if(cell[STATUS_VALUE]= =FLAGGED)
@@ -10,12 +17,6 @@ package src.main.java.name.intentionrevealingname.cleancode;
     return flaggedCells;
 }*/
 
-        /*We can go further and write a simple class for cells instead of using an array of ints. It can include an intention-revealing function
-        (call it isFlagged) to hide the magic numbers. It results in a new version of the function:*/
-
-
-
-public class NamesArrayList {
     public List<Cell> getFlaggedCells() {
         List<Cell> flaggedCells = new ArrayList<Cell>();
         for (Cell cell : gameBoard)

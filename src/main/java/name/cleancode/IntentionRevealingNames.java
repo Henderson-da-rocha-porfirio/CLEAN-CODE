225d0bd823f7410c7cbcbb2ce6eb6d662ff195d6
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-04-18 10:57:42 -0300

    Add files via upload

---
+++ src/main/java/name/cleancode/IntentionRevealingNames.java
@@ -0,0 +1,11 @@
+package src.main.java.name.cleancode;
+// Example using "d" variable.
+// The name d reveals nothing:
+// 1. It does not evoke a sense of elapsed time
+// 2. nor of days.
+
+public class IntentionRevealingNames {
+  
+  int d; // elapsed time in days
+  
+}

030f6690d2e69dfed2de150bf1bab49ebe9e4edd
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-04-18 10:41:10 -0300

    Update and rename IntentionRevealingNames.java to IntentionRevealingNamesNothing.java

---
+++ IntentionRevealingNamesNothing.java
@@ -0,0 +1,11 @@
+
+// Example using "d" variable.
+// The name d reveals nothing:
+// 1. It does not evoke a sense of elapsed time
+// 2. nor of days.
+
+public class IntentionRevealingNames {
+  
+  int d; // elapsed time in days
+  
+}

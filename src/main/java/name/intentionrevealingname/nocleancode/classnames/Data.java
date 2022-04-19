9f7de1bbfba59906342bee061e18115324097cfe
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-04-19 10:29:29 -0300

    #COMMIT 15 - cleancode

---
+++ src/main/java/name/intentionrevealingname/nocleancode/classnames/Data.java
@@ -0,0 +1,4 @@
+package src.main.java.name.intentionrevealingname.nocleancode.classnames;
+/*Classes and objects should have noun or noun phrase names. A class name should not be a verb.*/
+public class Data {
+}

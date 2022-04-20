80f899215720cf8ff20c067e13263069ed590a68
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-04-20 17:11:21 -0300

    #COMMIT 5 - cleancode - functions - if

--- src/main/java/functions/cleancode/ifs/NoUseIfs.java
+++ src/main/java/functions/cleancode/ifs/NoUseIfs.java
@@ -1,4 +1,10 @@
 package src.main.java.functions.cleancode.ifs;
+
+/*However, with just a few simple method extractions,
+        some renaming, and a little restructuring,
+        I was able to capture the intent of the function in the nine lines
+        See whether you can understand that*/
+
 public class NoUseIfs {
     public static String renderPageWithSetupsAndTeardowns(
             PageData pageData, boolean isSuite

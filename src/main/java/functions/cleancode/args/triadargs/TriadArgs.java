bb254ed61b7c8510009bf1405412e3b52f9a44ed
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-04-22 11:50:51 -0300

    #COMMIT 18 - cleancode - functions - args

---
+++ src/main/java/functions/cleancode/args/triadargs/TriadArgs.java
@@ -0,0 +1,15 @@
+package src.main.java.functions.cleancode.args.triadargs;
+
+/* On the other hand, here is a triad that is not quite so insidious: assertEquals( 1.0, amount, .001).
+Although this still requires a double-take, it’s one that’s worth taking.
+ It’s always good to be reminded that equality of floating point values is a relative thing.*/
+
+public class TriadArgs {
+
+    void assertEquals( 1.0, amount, .001) {
+
+
+    }
+
+
+}
---
+++ src/main/java/functions/nocleancode/args/triadargs/TriadArgs.java
@@ -0,0 +1,18 @@
+package src.main.java.functions.nocleancode.args.triadargs;
+
+import java.util.stream.Stream;
+
+/*For example, consider the common overload of assertEquals that takes three arguments: assertEquals( message, expected, actual).
+How many times have you read the message and thought it was the expected? I have stumbled and paused over that particular triad many times.
+ In fact, every time I see it, I do a double-take and then learn to ignore the message./
+ */
+
+public class TriadArgs {
+
+    void assertEquals(message, expected, actual) {
+
+
+    }
+
+
+}

68c1ddde5164b6f45be58964139253439004a718
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-04-23 15:38:07 -0300

    #COMMIT 19 - cleancode - functions - args

---
+++ src/main/java/functions/nocleancode/args/ObjectsArgs.java
@@ -0,0 +1,14 @@
+package src.main.java.functions.nocleancode.args;
+
+
+/*Reducing the number of arguments by creating objects out of them may seem like cheating, but it’s not.
+        When groups of variables are passed together, the way x and y are in the example above,
+        they are likely part of a concept that deserves a name of its own.*/
+
+import java.awt.*;
+
+public class ObjectsArgs {
+
+    Circle makeCircle(Point center, double radius);
+
+}

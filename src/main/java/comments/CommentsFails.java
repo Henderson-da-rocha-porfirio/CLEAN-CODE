eb6eab62d3074f2795a3d15b8c5c705452f38768
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-04-27 10:58:04 -0300

    #COMMIT 4 - cleancode - comments

--- src/main/java/comments/CommentsFails.java
+++ src/main/java/comments/CommentsFails.java
@@ -2,6 +2,8 @@ package src.main.java.comments;
 
 import java.util.Locale;
 
+/*And all too often the comments get separated from the code they describe and become orphaned blurbs of ever-decreasing accuracy.
+        For example, look what has happened to this comment and the line it was intended to describe:*/
 public class CommentsFails {
     MockRequest request;
     private final String HTTP_DATE_REGEXP =

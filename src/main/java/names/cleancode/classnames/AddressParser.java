85bcd4981c516f52238f69b59927d19093faae63
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-05-05 11:03:55 -0300

    #COMMIT 4 - cleancode - OBJECTS_and_DATA

--- src/main/java/names/cleancode/classnames/AddressParser.java
+++ src/main/java/names/cleancode/classnames/AddressParser.java
@@ -1,4 +1,4 @@
 package src.main.java.names.cleancode.classnames;
-/*Classes and dataandobjects should have noun or noun phrase names. A classes names should not be a verb.*/
+/*Classes and objects should have noun or noun phrase names. A classes names should not be a verb.*/
 public class AddressParser {
 }
--- src/main/java/objects/cleancode/dataabstraction/AbstractCaseVehicle.java
+++ src/main/java/objects/cleancode/dataabstraction/AbstractCaseVehicle.java
@@ -1,4 +1,4 @@
-package src.main.java.dataandobjects.cleancode.dataabstraction;
+package src.main.java.objects.cleancode.dataabstraction;
 
 /* 1. The second (the first is ConcreteCaseVehicle class) does so with the abstraction of percentage
 *  2. In this abstract case you have no clue at all about the form of the data.

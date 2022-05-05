aac64544d6d6b5757681647d127b8c141e61b517
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-05-05 10:27:50 -0300

    #COMMIT 2 - cleancode - OBJECTS_and_DATA

--- src/main/java/objects/cleancode/dataabstraction/AbstractCaseVehicle.java
+++ src/main/java/objects/cleancode/dataabstraction/AbstractCaseVehicle.java
@@ -1,7 +1,13 @@
 package src.main.java.objects.cleancode.dataabstraction;
 
 /* 1. The second (the first is ConcreteCaseVehicle class) does so with the abstraction of percentage
-*  2. In this abstract case you have no clue at all about the form of the data.*/
+*  2. In this abstract case you have no clue at all about the form of the data.
+*  3. In both of the above cases the second option is preferable.
+*  4. We don't want to expose the details of our data.
+*  5. Rather we want to express our data in abstract terms.
+*  6. This is not merely accomplished by using interfaces and/or getters and setters.
+*  7. Serious thought needs to be put into the best way to represent the data that an object contains.
+*  8. The worst option is to blithely add getters and setters.*/
 
 public interface AbstractCaseVehicle {
 

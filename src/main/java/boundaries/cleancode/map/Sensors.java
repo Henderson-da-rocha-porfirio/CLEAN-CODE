bc499e4ae6b4a8a0d0a5cab29be72fe0fea771df
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-06-27 15:43:49 -0300

    # 3 - Commit - BOUNDARIES

--- src/main/java/boundaries/cleancode/map/Sensors.java
+++ src/main/java/boundaries/cleancode/map/Sensors.java
@@ -7,17 +7,28 @@ import java.util.map;
 
   /*A cleaner way to use Map might look like the following.
  No user of Sensors would care one bit if generics were used or not.
- That choice has become (and always should be) an implementation detail.*/
+ That choice has become (and always should be) an implementation detail.
+
+  The interface at the boundary (Map) is hidden. It is able to evolve with very little impact on the rest of the application.
+  The use of generics is no longer a big issue because the casting and type management is handled inside the Sensors class.
+  This interface is also tailored and constrained to meet the needs of the application.
+  It results in code that is easier to understand and harder to misuse.
+  The Sensors class can enforce design and business rules.
+  We are not suggesting that every use of Map be encapsulated in this form.
+  Rather, we are advising you not to pass Maps (or any other interface at a boundary) around your system.
+  If you use a boundary interface like Map, keep it inside the class, or close family of classes, where it is used.
+  Avoid returning it from, or accepting it as an argument to, public APIs.*/
+
 
 public class Sensors {
 
-        private java.util.Map sensors = new HashMap();
+    private java.util.Map sensors = new HashMap();
 
-        public Sensor getById(String id) {
-            return (Sensor) sensors.get(id);
-        }
+    public Sensor getById(String id) {
+        return (Sensor) sensors.get(id);
+    }
          ...
 
 
-        // snip
+    // snip
 }
